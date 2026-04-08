a42fb53a3793f4c3e9a25997d979e143d4d3e5f2
ANKIT KUMAR GUPTA <ankitforcoding@gmail.com>
2026-04-08 18:18:10 +0530

    Time: 54 ms (5.76%), Space: 53.5 MB (67.93%) - LeetHub

---
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -0,0 +1,15 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+
+    let a = args.length;
+
+    return a ; 
+    
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
