9ac14282bfc40d4f6d69f23aface49a1a7641968
ANKIT KUMAR GUPTA <ankitforcoding@gmail.com>
2026-04-30 11:02:49 +0530

    Time: 25 ms (96.65%), Space: 53.5 MB (69.41%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void solve(string& s , int i , vector<vector<string>>& result , vector<string>&curr , vector<vector<bool>>&t){
+
+        if(i == s.length()){
+            result.push_back(curr);
+            return ; 
+        }
+
+        for(int j = i ; j < s.length() ; j++){
+            if(t[i][j] == true){
+                curr.push_back(s.substr(i, j - i + 1));
+                solve(s , j + 1 , result , curr , t);
+                curr.pop_back(); 
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+
+        // APPROACH : BOTTOM UP(BLUE PRINT)
+
+        int n = s.length(); 
+
+        vector<vector<bool>>t(n , vector<bool>(n)); 
+
+        for(int i = 0 ; i < n ; i++){
+            t[i][i] = true; 
+        }
+
+        for(int L  = 2 ; L <= n ; L++){
+            for(int i = 0 ; i < n-L+1; i++){
+                int j = i + L - 1 ; 
+
+                if(s[i] == s[j]){
+                    if(L == 2){
+                    t[i][j] = true; }
+                   else{
+                       t[i][j] = t[i+1][j-1];
+                }
+                }
+            }
+        }
+
+        vector<vector<string>> result; 
+        vector<string>curr ; 
+
+        solve(s , 0 , result , curr , t);
+
+        return result ; 
+        
+    }
+};
