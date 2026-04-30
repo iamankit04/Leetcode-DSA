9f1aa872816b70e969b4a9314ad28bb365e10e06
ANKIT KUMAR GUPTA <ankitforcoding@gmail.com>
2026-04-30 09:35:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    int t[1001][1001];
-    bool check(string &s , int i , int j ){
-
-        if(i > j) return true ; 
-
-        if(t[i][j] != -1) return t[i][j]; 
-
-        if(s[i] == s[j]) return t[i][j] =  check(s , i+1 , j-1);
-
-        return false ; 
-    }
     int countSubstrings(string s) {
 
-        // Recur + memo
-
-        memset(t , -1 , sizeof(t));
-
+        // bootom up 
         int n = s.length(); 
 
         int cnt = 0 ; 
 
-        for(int i = 0 ;  i < n; i++){
-            for(int j =  i ; j<n;  j++){
+        vector<vector<bool>> t(n , vector<bool>(n , false)); 
 
-                if(check(s , i , j)){
+        for(int l = 1 ; l <= n ; l++){
+
+            for(int i =  0 ; i + l - 1 < n ; i++){
+                int j = i + l  - 1 ; 
+
+                if(i == j){
+                    t[i][j] =  true ; 
+                }else if(i + 1  == j){
+                    t[i][j] = (s[i] == s[j]);
+                }else{
+                    t[i][j] =  (s[i] == s[j] && t[i+1][j-1]);
+                }
+
+                if(t[i][j] == true){
                     cnt++;
                 }
             }
         }
+
         return cnt ; 
+        
     }
 };
