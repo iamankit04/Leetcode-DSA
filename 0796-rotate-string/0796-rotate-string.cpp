d921b259091c350699658976e17c544ba05f4c4c
ANKIT KUMAR GUPTA <ankitforcoding@gmail.com>
2026-05-03 08:57:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        if(s.length() != goal.length()) return false ; 
+
+        string s1 = s + s ; 
+
+        if(s1.find(goal) != string::npos) return true ; 
+
+        return false ; 
+    }
+};
