d45472b1e288855100a5a015900c220048351745
ANKIT KUMAR GUPTA <ankitforcoding@gmail.com>
2026-05-04 13:36:08 +0530

    Time: 0 ms (100%), Space: 10.2 MB (35.34%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        // transpose + reverse 
+
+        int n = matrix.size();
+        int m = matrix[0].size(); 
+
+        //transpose
+        for(int i = 0 ; i < n;  i++){
+            for(int j = i; j < m; j++){
+                swap(matrix[i][j] , matrix[j][i]);
+            }
+        }
+        
+        //reverse
+        for(int i = 0 ;  i < n ; i++){
+            reverse(begin(matrix[i]) , end(matrix[i]));
+        }
+        
+    }
+};
