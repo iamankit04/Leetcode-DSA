1a0a895425cf0929e816cf16fc98fb6b0b993cd7
ANKIT KUMAR GUPTA <ankitforcoding@gmail.com>
2026-05-29 14:57:04 +0530

    Time: 59 ms (7.7%), Space: 10.9 MB (98.49%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int m , n ; 
     int t[501][501];
-    int solve(string &word1, string &word2 , int i , int j){
+    int solve(string &word1, string &word2 , int m , int n){
 
-        if(i >= m ) return n - j  ;
-        if(j >= n) return m - i ;  
+       if(m == 0 || n == 0) return m + n ;  
         
-        if(t[i][j] != -1) return t[i][j];
-        if(word1[i] == word2[j] ){
-            return t[i][j] =  solve(word1 , word2 , i + 1 , j + 1);
+        if(t[m][n] != -1) return t[m][n];
+        if(word1[m-1] == word2[n-1] ){
+            return t[m][n] =  solve(word1 , word2 , m -1 , n - 1);
         }  
 
-        return t[i][j] =  1 + min(solve(word1 , word2 , i + 1 , j) , solve(word1 , word2 , i , j +1)); 
+        return t[m][n] =  1 + min(solve(word1 , word2 , m - 1 , n) , solve(word1 , word2 , m , n - 1)); 
     }
     int minDistance(string word1, string word2) {
 
@@ -21,7 +20,7 @@ public:
 
         memset(t , -1 , sizeof(t));
 
-        return solve(word1 , word2 , 0 , 0);
+        return solve(word1 , word2 , m  , n  );
         
     }
 };
