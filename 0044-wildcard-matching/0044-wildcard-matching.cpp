a40376ea56377c248c98d180381c3580cc7931ac
ANKIT KUMAR GUPTA <ankitforcoding@gmail.com>
2026-07-15 16:57:03 +0530

    Time: 1271 ms (5.01%), Space: 26.8 MB (47.07%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int t[2001][2001] ; 
-    bool solve(string& s , string& p , int m , int n ){
+    int t[2001][2001];
+    bool solve(string &s , string&p , int m , int n){
 
         if(m < 0 && n < 0) return true; 
-
         if(m >= 0 && n < 0) return false ; 
 
         if(m < 0 && n >= 0){
-
-            for(int k = n ; k >= 0; k--){
+            for(int k = n ; k >= 0 ; k--){
                 if(p[k] != '*') return false ; 
             }
 
             return true ; 
         }
 
-        if(t[m][n] != -1) return t[m][n];
+        if(t[m][n] != -1) return t[m][n]; 
 
         if(s[m] == p[n] || p[n] == '?'){
-            return t[m][n] =  solve(s , p , m - 1 , n - 1);
+            return t[m][n] = solve(s , p , m - 1, n - 1 );
         }else if(p[n] == '*'){
-             return t[m][n] =  solve(s , p , m - 1 , n) || solve(s , p , m , n - 1);
+            return t[m][n] =  solve(s , p , m - 1 , n) || solve(s , p , m , n - 1); 
         }
 
         return false ; 
@@ -31,9 +29,9 @@ public:
         int m = s.length(); 
         int n = p.length(); 
 
-        memset(t , -1 , sizeof(t));
+        memset(t , -1 , sizeof(t)); 
 
-        return solve(s , p , m - 1 , n - 1); 
+        return solve(s , p , m - 1 ,  n - 1);
         
     }
 };
