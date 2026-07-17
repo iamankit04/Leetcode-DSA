ae74c46878e1b8f2e38d65859e8e2b5adf0d07ba
ANKIT KUMAR GUPTA <cse23071@glbitm.ac.in>
2026-07-18 00:28:03 +0530

    Time: 3 ms (54.35%), Space: 10.8 MB (80.57%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,33 +2,27 @@ class Solution {
 public:
     string frequencySort(string s) {
 
-        unordered_map<char , int> mp ; 
+        unordered_map<char ,int > mp ; 
 
         for(auto &it : s){
-            mp[it]++;
+            mp[it]++; 
         }
 
-        priority_queue<pair<int , char>> pq ; 
+        vector<pair<int , char >> p ; 
 
         for(auto &it : mp){
-            pq.push({it.second , it.first}); 
+            p.push_back({it.second , it.first});
         }
+
+        sort(begin(p) , end(p) , greater<pair<int , char>>());
 
         string ans = ""; 
 
-        while(!pq.empty()){
+        for(auto &it : p){
 
-            auto curr = pq.top();
-            pq.pop(); 
-
-            char ch = curr.second; 
-            int k = curr.first ; 
-
-            while(k--){
-                ans += ch; 
-            } 
-
-            
+            while(it.first--){
+                ans += it.second; 
+            }
         }
         return ans ; 
     }
