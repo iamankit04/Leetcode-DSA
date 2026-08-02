00624eeb264c2f946dae279f209fabe9cbe168c7
ANKIT KUMAR GUPTA <cse23071@glbitm.ac.in>
2026-08-02 10:19:06 +0530

    Time: 7 ms (35.23%), Space: 11.7 MB (34.08%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int t[501][501]; 
+    int solve(int i , int j , vector<int>& piles){
+
+        if(i == j) return piles[i]; 
+
+        if(t[i][j] != -1) return t[i][j]; 
+
+        int l = piles[i] - solve(i + 1 , j ,piles ); 
+        int r = piles[j] - solve(i , j - 1 , piles); 
+
+        return t[i][j] =  max(l , r); 
+    }
+    bool stoneGame(vector<int>& piles) {
+
+        int n = piles.size();
+
+        memset(t , -1 , sizeof(t));  
+
+        return solve(0 , n - 1 , piles) >= 0; 
+        
+    }
+};
