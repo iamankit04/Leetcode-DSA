5eb7090054c3a6e054484463270f0caac0ede1b7
ANKIT KUMAR GUPTA <cse23071@glbitm.ac.in>
2026-08-06 21:41:49 +0530

    Time: 825 ms (21.32%), Space: 11.6 MB (37.29%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
     int m , n ; 
-    vector<vector<int>> directions = {{1 , 0} , {-1 , 0 } , {0, 1} , {0, -1}}; 
-    bool find(vector<vector<char>>& board, string word , int i , int j , int idx){
+    bool dfs(vector<vector<char>>& board, string word , int i , int j , int idx){
 
-        if(idx == word.length()) return true ; 
+        if(idx == word.size()) return true; 
 
-        if(i >= m || i < 0 || j >= n || j < 0 || board[i][j] != word[idx] || board[i][j] == '#') return false ; 
+        if(i >= m || i < 0 || j >= n || j < 0 || board[i][j] != word[idx]) return false ; 
 
-        char temp = board[i][j] ; 
-        board[i][j] = '#' ; 
+        char temp = board[i][j]; 
+        board[i][j] = '#'; 
 
-        for(auto &dir : directions){
-            int i_ = i + dir[0]; 
-            int j_ = j + dir[1] ; 
-
-            if(find(board , word , i_ , j_ , idx + 1)) return true; 
-        }
-
+        bool found = dfs(board , word , i + 1 , j , idx + 1) || dfs(board , word , i - 1 , j , idx + 1) || dfs(board , word , i , j - 1 , idx + 1) || dfs(board , word , i  , j + 1 , idx + 1) ; 
 
         board[i][j] = temp ; 
 
-
-        return false ; 
+        return found ; 
     }
     bool exist(vector<vector<char>>& board, string word) {
-
-        //Backtracking
-
         m = board.size(); 
         n = board[0].size(); 
 
-        for(int i = 0 ;  i < m ; i++){
+        for(int i = 0 ; i  < m ; i++){
             for(int j = 0 ; j  < n ; j++){
 
-                if(board[i][j] == word[0] && find(board , word , i , j , 0))
-                         return true ; 
+                if(board[i][j] == word[0] && dfs(board , word , i , j , 0)){
+                    return true ; 
+                }
             }
         }
-
         return false ; 
         
     }
