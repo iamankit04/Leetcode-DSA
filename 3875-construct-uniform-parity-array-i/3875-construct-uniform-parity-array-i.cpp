61e7689f60f20f5cec865a99776263f03fce9c4c
ANKIT KUMAR GUPTA <cse23071@glbitm.ac.in>
2026-09-03 14:00:19 +0530

    Time: 0 ms (100%), Space: 30.2 MB (95.03%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,22 +1,6 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-
-        int n= nums1.size();
-
-        int cnt1 = 0 , cnt2 = 0 ;
-        
-        if(n == 0) return false; 
-
-        for(int i = 0 ; i < n ;i++){
-            if(nums1[i] == nums1[0]){
-                cnt1++;
-            }else{
-                cnt2++;
-            }
-        }
-
-        return cnt1 == 1 || cnt2 == 1 || cnt1 == 0 || cnt2 == 0 ;   
-        
+        return true ; 
     }
 };
